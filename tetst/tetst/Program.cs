9b13a2366d347a02869cb6a32bb8a427187d9aeb
garikmogilev <garikmogilev@gmail.com>
2021-04-15 12:49:05 +0300

    add_tests

--- tetst/tetst/Program.cs
+++ tetst/tetst/Program.cs
@@ -1,163 +1,119 @@
 ﻿using System;
-using System.Collections.Generic;
 
-namespace RefactoringGuru.DesignPatterns.Builder.Conceptual
+namespace tetst
 {
-    // Интерфейс Строителя объявляет создающие методы для различных частей
-    // объектов Продуктов.
-    public interface IBuilder
+    // Базовый интерфейс Компонента определяет поведение, которое изменяется
+    // декораторами.
+    public abstract class Component
     {
-        void BuildPartA();
-        
-        void BuildPartB();
-        
-        void BuildPartC();
+        public abstract string Operation();
     }
-    
-    // Классы Конкретного Строителя следуют интерфейсу Строителя и предоставляют
-    // конкретные реализации шагов построения. Ваша программа может иметь
-    // несколько вариантов Строителей, реализованных по-разному.
-    public class ConcreteBuilder : IBuilder
+
+    // Конкретные Компоненты предоставляют реализации поведения по умолчанию.
+    // Может быть несколько вариаций этих классов.
+    class ConcreteComponent : Component
     {
-        private Product _product = new Product();
-        
-        // Новый экземпляр строителя должен содержать пустой объект продукта,
-        // который используется в дальнейшей сборке.
-        public ConcreteBuilder()
+        public override string Operation()
         {
-            this.Reset();
-        }
-        
-        public void Reset()
-        {
-            this._product = new Product();
-        }
-        
-        // Все этапы производства работают с одним и тем же экземпляром
-        // продукта.
-        public void BuildPartA()
-        {
-            this._product.Add("PartA1");
-        }
-        
-        public void BuildPartB()
-        {
-            this._product.Add("PartB1");
-        }
-        
-        public void BuildPartC()
-        {
-            this._product.Add("PartC1");
-        }
-        
-        // Конкретные Строители должны предоставить свои собственные методы
-        // получения результатов. Это связано с тем, что различные типы
-        // строителей могут создавать совершенно разные продукты с разными
-        // интерфейсами. Поэтому такие методы не могут быть объявлены в базовом
-        // интерфейсе Строителя (по крайней мере, в статически типизированном
-        // языке программирования).
-        //
-        // Как правило, после возвращения конечного результата клиенту,
-        // экземпляр строителя должен быть готов к началу производства
-        // следующего продукта. Поэтому обычной практикой является вызов метода
-        // сброса в конце тела метода GetProduct. Однако такое поведение не
-        // является обязательным, вы можете заставить своих строителей ждать
-        // явного запроса на сброс из кода клиента, прежде чем избавиться от
-        // предыдущего результата.
-        public Product GetProduct()
-        {
-            Product result = this._product;
-
-            this.Reset();
-
-            return result;
+            return "ConcreteComponent";
         }
     }
-    
-    // Имеет смысл использовать паттерн Строитель только тогда, когда ваши
-    // продукты достаточно сложны и требуют обширной конфигурации.
-    //
-    // В отличие от других порождающих паттернов, различные конкретные строители
-    // могут производить несвязанные продукты. Другими словами, результаты
-    // различных строителей  могут не всегда следовать одному и тому же
-    // интерфейсу.
-    public class Product
-    {
-        private List<object> _parts = new List<object>();
-        
-        public void Add(string part)
-        {
-            this._parts.Add(part);
-        }
-        
-        public string ListParts()
-        {
-            string str = string.Empty;
 
-            for (int i = 0; i < this._parts.Count; i++)
+    // Базовый класс Декоратора следует тому же интерфейсу, что и другие
+    // компоненты. Основная цель этого класса - определить интерфейс обёртки для
+    // всех конкретных декораторов. Реализация кода обёртки по умолчанию может
+    // включать в себя  поле для хранения завёрнутого компонента и средства его
+    // инициализации.
+    abstract class Decorator : Component
+    {
+        protected Component _component;
+
+        public Decorator(Component component)
+        {
+            this._component = component;
+        }
+
+        public void SetComponent(Component component)
+        {
+            this._component = component;
+        }
+
+        // Декоратор делегирует всю работу обёрнутому компоненту.
+        public override string Operation()
+        {
+            if (this._component != null)
             {
-                str += this._parts[i] + ", ";
+                return this._component.Operation();
             }
+            else
+            {
+                return string.Empty;
+            }
+        }
+    }
 
-            str = str.Remove(str.Length - 2); // removing last ",c"
+    // Конкретные Декораторы вызывают обёрнутый объект и изменяют его результат
+    // некоторым образом.
+    class ConcreteDecoratorA : Decorator
+    {
+        public ConcreteDecoratorA(Component comp) : base(comp)
+        {
+        }
 
-            return "Product parts: " + str + "\n";
+        // Декораторы могут вызывать родительскую реализацию операции, вместо
+        // того, чтобы вызвать обёрнутый объект напрямую. Такой подход упрощает
+        // расширение классов декораторов.
+        public override string Operation()
+        {
+            return $"ConcreteDecoratorA({base.Operation()})";
+        }
+    }
+
+    // Декораторы могут выполнять своё поведение до или после вызова обёрнутого
+    // объекта.
+    class ConcreteDecoratorB : Decorator
+    {
+        public ConcreteDecoratorB(Component comp) : base(comp)
+        {
+        }
+
+        public override string Operation()
+        {
+            return $"ConcreteDecoratorB({base.Operation()})";
         }
     }
     
-    // Директор отвечает только за выполнение шагов построения в определённой
-    // последовательности. Это полезно при производстве продуктов в определённом
-    // порядке или особой конфигурации. Строго говоря, класс Директор
-    // необязателен, так как клиент может напрямую управлять строителями.
-    public class Director
+    public class Client
     {
-        private IBuilder _builder;
-        
-        public IBuilder Builder
+        // Клиентский код работает со всеми объектами, используя интерфейс
+        // Компонента. Таким образом, он остаётся независимым от конкретных
+        // классов компонентов, с которыми работает.
+        public void ClientCode(Component component)
         {
-            set { _builder = value; } 
-        }
-        
-        // Директор может строить несколько вариаций продукта, используя
-        // одинаковые шаги построения.
-        public void buildMinimalViableProduct()
-        {
-            this._builder.BuildPartA();
-        }
-        
-        public void buildFullFeaturedProduct()
-        {
-            this._builder.BuildPartA();
-            this._builder.BuildPartB();
-            this._builder.BuildPartC();
+            Console.WriteLine("RESULT: " + component.Operation());
         }
     }
-
+    
     class Program
     {
-        private static void Main(string[] args)
+        static void Main(string[] args)
         {
-            // Клиентский код создаёт объект-строитель, передаёт его директору,
-            // а затем инициирует  процесс построения. Конечный результат
-            // извлекается из объекта-строителя.
-            var director = new Director();
-            var builder = new ConcreteBuilder();
-            director.Builder = builder;
-            
-            Console.WriteLine("Standard basic product:");
-            director.buildMinimalViableProduct();
-            Console.WriteLine(builder.GetProduct().ListParts());
+            Client client = new Client();
 
-            Console.WriteLine("Standard full featured product:");
-            director.buildFullFeaturedProduct();
-            Console.WriteLine(builder.GetProduct().ListParts());
+            var simple = new ConcreteComponent();
+            Console.WriteLine("Client: I get a simple component:");
+            client.ClientCode(simple);
+            Console.WriteLine();
 
-            // Помните, что паттерн Строитель можно использовать без класса
-            // Директор.
-            Console.WriteLine("Custom product:");
-            builder.BuildPartA();
-            builder.BuildPartC();
-            Console.Write(builder.GetProduct().ListParts());
+            // ...так и декорированные.
+            //
+            // Обратите внимание, что декораторы могут обёртывать не только
+            // простые компоненты, но и другие декораторы.
+            ConcreteDecoratorA decorator1 = new ConcreteDecoratorA(simple);
+            ConcreteDecoratorB decorator2 = new ConcreteDecoratorB(decorator1);
+            Console.WriteLine("Client: Now I've got a decorated component:");
+            client.ClientCode(decorator1);
+            client.ClientCode(decorator2);
         }
     }
 }
